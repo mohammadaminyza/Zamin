1676ef715f42e8328d023964e509e06e2cb8e69d
60146 <m.yaghoubzadeh@saapp.ir>
2022-05-23 18:45:00 +0430

    Add EventUserInfo For Getting User Event Infomantion

---
+++ OldStructure/src/02. Core/Zamin.Core.Domain/Events/EventUserInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace Zamin.Core.Domain.Events;
+
+public class EventUserInfo
+{
+    public static string? UserId { get; set; }
+    public static string? UserIp { get; set; }
+    public static string? UserAgent { get; set; }
+}
