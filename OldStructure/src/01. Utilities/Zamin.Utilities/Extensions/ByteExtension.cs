83420f5869784c09f1819f3c25c576961046d77e
60146 <m.yaghoubzadeh@saapp.ir>
2022-05-23 18:44:11 +0430

    Add ByteExtension

---
+++ OldStructure/src/01. Utilities/Zamin.Utilities/Extensions/ByteExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace Zamin.Utilities.Extensions;
+
+public static class ByteExtension
+{
+    public static string? ByteArrayToString(this byte[]? value)
+    {
+        if (value == null)
+            return null;
+
+        return Encoding.UTF8.GetString(value);
+    }
+}
